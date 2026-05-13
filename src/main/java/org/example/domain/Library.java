9692debc08748fbc0aebffac280dc7fe7c7b5521
2583626 <2583626@edu.vaniercollege.qc.ca>
2026-05-13 16:47:43 -0400

    Constructor with ArrayList

--- src/main/java/org/example/domain/Library.java
+++ src/main/java/org/example/domain/Library.java
@@ -1,8 +1,14 @@
 package org.example.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
     private List<Item> items;
     private List<User> users;
+
+    public Library() {
+        this.items = new ArrayList<>();
+        this.users = new ArrayList<>();
+    }
 }
