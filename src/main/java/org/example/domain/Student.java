e5b9f0380f8bd387a9203a9268028ccd88a9118a
2583626 <2583626@edu.vaniercollege.qc.ca>
2026-05-13 16:26:31 -0400

    Added student class

---
+++ src/main/java/org/example/domain/Student.java
@@ -0,0 +1,12 @@
+package org.example.domain;
+
+public class Student extends User {
+    public Student(String name) {
+        super(name);
+    }
+
+    @Override
+    public int getBorrowLimit() {
+        return 5;
+    }
+}
