632967c4a3fc9b2ec74196e71a2582222029df27
2583626 <2583626@edu.vaniercollege.qc.ca>
2026-05-13 16:43:12 -0400

    Added static boolean isNotEmpty and isPositive

--- src/main/java/org/example/util/Validation.java
+++ src/main/java/org/example/util/Validation.java
@@ -4,4 +4,12 @@ public class Validation {
     public static boolean isValidISBN(String isbn) {
         return isbn.matches("^9 [78)\\d{13}");
     }
+
+    public static boolean isNotEmpty(String value) {
+        return value != null && !value.trim().isEmpty();
+    }
+
+    public static boolean isPositive(int value) {
+        return value > 0;
+    }
 }
