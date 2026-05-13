96a26e3a8fe207a7cf3cd7209e2608aaf8c990ad
2583626 <2583626@edu.vaniercollege.qc.ca>
2026-05-13 17:35:51 -0400

    Added a sortUsersByName and had to import User class

--- src/main/java/org/example/service/LibraryService.java
+++ src/main/java/org/example/service/LibraryService.java
@@ -1,6 +1,7 @@
 package org.example.service;
 
 import org.example.domain.Item;
+import org.example.domain.User;
 
 import java.util.Comparator;
 import java.util.List;
@@ -16,6 +17,12 @@ public class LibraryService {
     public List<Item> sortItemsByStatus(List<Item> items) {
         return items.stream()
                 .sorted(Comparator.comparing(item -> item.getStatus().name()))
+                .toList();
+    }
+
+    public List<User> sortUsersByName(List<User> users) {
+        return users.stream()
+                .sorted(Comparator.comparing(User::getName))
                 .toList();
     }
 }
