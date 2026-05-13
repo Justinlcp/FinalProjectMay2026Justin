fad9671bacf4a38d958580b4900ac1e74b0e4e61
2583626 <2583626@edu.vaniercollege.qc.ca>
2026-05-13 17:52:12 -0400

    Fixed Reportable to match

--- src/main/java/org/example/interfaces/Reportable.java
+++ src/main/java/org/example/interfaces/Reportable.java
@@ -1,5 +1,9 @@
 package org.example.interfaces;
 
+import org.example.domain.Item;
+
+import java.util.List;
+
 public interface Reportable {
-    void generateReport();
+    void generateReport(List<Item> items);
 }
