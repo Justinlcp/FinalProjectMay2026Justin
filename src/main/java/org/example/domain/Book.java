c7ee87b4f7a142e5cc749b567686cea0a0bd4b3b
2583626 <2583626@edu.vaniercollege.qc.ca>
2026-05-13 15:53:57 -0400

    Added constructor with super

--- src/main/java/org/example/domain/Book.java
+++ src/main/java/org/example/domain/Book.java
@@ -4,4 +4,11 @@ public class Book extends Item {
     private String isbn;
     private String author;
     private String genre;
+
+    public Book(String title, ItemStatus status, String isbn, String author, String genre) {
+        super(title, status);
+        this.isbn = isbn;
+        this.author = author;
+        this.genre = genre;
+    }
 }
