f1361505478bde4c600199a0e969d01741992915
2583626 <2583626@edu.vaniercollege.qc.ca>
2026-05-13 15:47:40 -0400

    Added variables getter setter

--- src/main/java/org/example/domain/Item.java
+++ src/main/java/org/example/domain/Item.java
@@ -1,4 +1,14 @@
 package org.example.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public abstract class Item {
+    protected String id;
+    protected String title;
+    protected ItemStatus status;
+
+    private static int nextId = 1;
 }
