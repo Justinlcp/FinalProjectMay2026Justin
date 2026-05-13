6b05d16bca22f3cdc5308e848746f6a1330d55aa
2583626 <2583626@edu.vaniercollege.qc.ca>
2026-05-13 16:04:40 -0400

    added variables and constructor

--- src/main/java/org/example/domain/Magazine.java
+++ src/main/java/org/example/domain/Magazine.java
@@ -1,4 +1,12 @@
 package org.example.domain;
 
-public class Magazine {
+public class Magazine extends Item {
+        private int issueNumber;
+        private String publisher;
+
+        public Magazine(String title, ItemStatus status, int issueNumber, String publisher) {
+            super(title, status);
+            this.issueNumber = issueNumber;
+            this.publisher = publisher;
+        }
 }
