dd97d44bc5d1359bdfa02bd91dd416cd0dc85364
2583626 <2583626@edu.vaniercollege.qc.ca>
2026-05-14 16:22:32 -0400

    Added my /** in Student

--- src/main/java/org/example/domain/Student.java
+++ src/main/java/org/example/domain/Student.java
@@ -5,6 +5,10 @@ public class Student extends User {
         super(name);
     }
 
+    /**
+     * Returns the borrow limit for a student
+     * @return 5 as the maximum number of books a student can borrow
+     */
     @Override
     public int getBorrowLimit() {
         return 5;
