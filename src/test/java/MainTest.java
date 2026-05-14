acb3d008ca77e2b189ea5304c93c43c4a1b2b742
2583626 <2583626@edu.vaniercollege.qc.ca>
2026-05-14 14:44:21 -0400

    Test for BorrowItem

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,7 +2,11 @@ import org.example.domain.Book;
 import org.example.domain.ItemStatus;
 import org.example.domain.Library;
 import org.example.domain.Student;
+import org.example.exceptions.BorrowLimitException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class MainTest {
 
@@ -17,5 +21,19 @@ public class MainTest {
         book = new Book("Java Book", ItemStatus.IN_STORE, "1234567890123", "John", "Education");
         library.addUser(student);
         library.addItem(book);
+    }
+
+    @Test
+    @DisplayName("Student successfully borrows a book")
+    void testBorrowItem1() throws BorrowLimitException, ItemNotAvailableException {
+        library.borrowItem(student, book);
+        Assertions.assertEquals(ItemStatus.BORROWED, book.getStatus());
+    }
+
+    @Test
+    @DisplayName("Borrowed items list increases after borrowing")
+    void testBorrowItem2() throws BorrowLimitException, ItemNotAvailableException {
+        library.borrowItem(student, book);
+        Assertions.assertEquals(1, student.getBorrowedItems().size());
     }
 }
