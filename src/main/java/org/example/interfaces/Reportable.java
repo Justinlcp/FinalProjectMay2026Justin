cc5effd2e4dbb54326492ddedc08d1b59e0237a3
2583626 <2583626@edu.vaniercollege.qc.ca>
2026-05-14 16:24:29 -0400

    Added my /** in Reportable

--- src/main/java/org/example/interfaces/Reportable.java
+++ src/main/java/org/example/interfaces/Reportable.java
@@ -5,5 +5,9 @@ import org.example.domain.Item;
 import java.util.List;
 
 public interface Reportable {
+    /**
+     * Generates a report of all items in the library
+     * @param items the list of items to report on
+     */
     void generateReport(List<Item> items);
 }
